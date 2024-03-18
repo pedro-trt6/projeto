93d66e4d88058abe3e2783bea3828578a431ef1b
Pedro <pedro.barbosa@trt6.jus.br>
2024-03-18 12:48:54 -0300

    Nova classe

--- src/main/java/br/cnj/projeto/CasoJudicialController.java
+++ src/main/java/br/cnj/projeto/CasoJudicialController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
